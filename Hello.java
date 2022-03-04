f51b9a19ea4316150c2a85fe96c4d2ac766a2e32
sanjay jadhav <sanjay1206090@gmail.com>
2022-03-05 00:09:21 +0530

    Commit in hello from VS code

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Word");
 	System.out.println("Testing GitHub");
+	System.out.println("Testing GitHUb for VS Code");
     }
 }
